6c60801231a8a676f87e623042927c63ec1dd8e0
alban <albanguyon@yahoo.fr>
2023-02-01 17:49:07 -0500

    check time with server

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, request
 from gui import viewBoard
 from moves import getAllLegalMoves
 from string import ascii_letters
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -13,13 +14,25 @@ def convert_to_tuple_coordinate(coord:str) -> tuple[int, int]:
     letter, number = coord
     
     x = ascii_letters.index(letter)
-    y = 8 - int(number)
+    # black's a file rook is (0, 0), 
+    # so that when the board is rendered, we see white's perspective
+    y = 8 - int(number) 
     assert x in range(8) and y in range(8)
 
     # the grid is a list of lists so it sees the board flipped
     return y, x
-    
 
+@app.route('/gettimeleft', methods=['POST'])
+def gettimeleft():
+    """return the amount of seconds left for the player"""
+    player = int(request.form['player'])
+
+    if player == 1:
+        return str((p1_time - datetime.now()).seconds)
+    if player == 2:
+        return str((p1_time - datetime.now()).seconds)
+
+    return 0
 
 @app.route('/submitmove', methods=['POST'])
 def submitmove():
@@ -29,17 +42,21 @@ def submitmove():
 
     print(list(request.form.values()))
 
-    print(f'Moving from {form_coord} to {to_coord}')
-
     from_tuple = convert_to_tuple_coordinate(form_coord)
     to_tuple = convert_to_tuple_coordinate(to_coord)
 
     new_move = (from_tuple, to_tuple)
     legal_moves = getAllLegalMoves(fen)
 
-    assert new_move in legal_moves
+    if new_move not in legal_moves:
+        return "illegal move", 409
+    
+    print(f'Moving from {form_coord} to {to_coord}')
     return "move submitted"
 
+
+p1_time = datetime.now() + timedelta(minutes=20)
+p2_time = datetime.now() + timedelta(minutes=20)
 
 fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 viewBoard(fen)
--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import requests
 
-url = "http://localhost:5000/submitmove"
+# url = "http://localhost:5000/submitmove"
+
+# data = {
+#     "from": 'e2',
+#     "to": 'e4'
+# }
+
+# r = requests.post(url, data)
+
+# print(r.status_code)
+# print(r.headers)
+# print(r.text)
+
+url = "http://localhost:5000/gettimeleft"
 
 data = {
-    "from": 'e2',
-    "to": 'e4'
+    "player": 1
 }
 
 r = requests.post(url, data)
+print(r.status_code)
+print(r.headers)
+print(r.text)
