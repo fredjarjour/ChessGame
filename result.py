f5ce3ac6d128e1d1ed994e414336a861a8b9048e
Frederic Jarjour <frederic.jarjour@umontreal.ca>
2023-02-03 01:07:57 -0500

    added files to track

---
+++ result.py
@@ -0,0 +1,24 @@
+from moves import isCheck, getAllLegalMoves
+from fen import fenToGrid
+import threefold
+
+# return a tuple: (isGameOver: bool, result: string explaining)
+def gameOver(fen):
+    if int(fen.split()[-2]) >= 100:
+        return (True, "50-move rule")
+    
+    if threefoldRepetition(fen):
+        return (True, "Threefold repetition")
+
+    # checkmate or stalemate
+    if getAllLegalMoves(fen) == []:
+        return (True, "Checkmate" if isCheck(fenToGrid(fen.split()[0]), fen.split()[1]) else "Stalemate")
+    
+    return (False, None)
+
+def threefoldRepetition(fen):
+    position = fen.split()[0]
+    if threefold.positions.get(position, 0) >= 2:
+        return True
+    threefold.positions[position] = threefold.positions.get(position, 0) + 1
+    return False
