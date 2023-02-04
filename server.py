7a809f64d17fa7b92818ae99f60a88b77caa7d94
alban <albanguyon@yahoo.fr>
2023-02-04 18:22:30 -0500

    major improvements to server

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, url_for, request
+from flask import Flask, request, session
 from gui import viewBoard
 from moves import getAllLegalMoves
 from fen import updateFen
 from string import ascii_letters
 from datetime import datetime, timedelta
+import markdown
 
 app = Flask(__name__)
+
+app.secret_key = 'mysecretkey'.encode()
 
 def convert_to_tuple_coordinate(coord:str) -> tuple[int, int]:
     """a1 becomes (0, 0) and b2 becomes (1, 1) ..."""
@@ -23,24 +26,56 @@ def convert_to_tuple_coordinate(coord:str) -> tuple[int, int]:
     # the grid is a list of lists so it sees the board flipped
     return y, x
 
+def is_player_turn(username):
+    turn = fen.split()[1] # gets 'w' if white's turn or 'b' if black's turn
+    if turn == 'w' and username == white_username:
+        return True
+    if turn == 'b' and username == black_username:
+        return True
+    return False
+
+@app.route('/login', methods=['POST'])
+def login():
+    global white_username, black_username
+    user = request.form['user']
+    session['username'] = user
+
+    # make sure there is not already 2 players
+    if white_username and black_username:
+        return "Game is already full", 423
+
+    if not white_username:
+        white_username = user
+    else:
+        black_username = user
+
+    return f"User {user} logged in successfully", 200
+
 
 @app.route('/gettimeleft', methods=['POST'])
 def gettimeleft():
     """return the amount of seconds left for the player"""
-    player = int(request.form['player'])
+    player = request.form['player'].lower()
 
-    if player == 1:
-        return str((p1_time - datetime.now()).seconds)
-    if player == 2:
-        return str((p1_time - datetime.now()).seconds)
+    if player == 'w':
+        return str((white_time - datetime.now()).seconds)
+    if player == 'b':
+        return str((white_time - datetime.now()).seconds)
 
-    return 0
+    return -1
 
 @app.route('/submitmove', methods=['POST'])
 def submitmove():
     global fen
+    if 'username' not in session:
+        return "You are not logged in", 401
 
-    if (p1_time - datetime.now()).seconds < 0:
+    print(f"Processing {session['username']}'s turn")
+
+    if not is_player_turn(session['username']):
+        return "Not your turn", 425
+
+    if (white_time - datetime.now()).seconds < 0:
         return "No time left on clock", 408
 
     # in the form a1 or b2 or h7 ...
@@ -48,8 +83,6 @@ def submitmove():
     to_coord = request.form['to']
     # which piece to promote to if the pawn reaches the end
     promote = request.form.get('promote', None)
-
-    print(list(request.form.values()))
 
     from_tuple = convert_to_tuple_coordinate(form_coord)
     to_tuple = convert_to_tuple_coordinate(to_coord)
@@ -68,12 +101,15 @@ def submitmove():
     fen = new_fen
     print(viewBoard(fen))
 
-    return "move submitted", 200
+    return f"move submitted by user {session['username']}", 200
 
-p1_time = datetime.now() + timedelta(minutes=20)
-p2_time = datetime.now() + timedelta(minutes=20)
+white_time = datetime.now() + timedelta(minutes=20)
+black_time = datetime.now() + timedelta(minutes=20)
+white_username = None
+black_username = None
 
-fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 p1"
-print(viewBoard(fen))
+fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
-app.run(debug=True)
+if __name__ == "__main__":
+    print(viewBoard(fen))
+    app.run(debug=False)
